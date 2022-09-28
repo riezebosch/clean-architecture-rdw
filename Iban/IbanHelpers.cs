a0b6e51a00d396a0f5da81102325b2ac6af5a151
Manuel Riezebosch <mriezebosch@gmail.com>
2022-09-28 08:11:24 +0000

    refactor(iban): check digits extension method

---
+++ Iban/IbanHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Iban
+{
+    internal static class IbanHelpers
+    {
+        public static bool CheckDigits(this string input)
+        {
+            return input.All(c => char.IsLetterOrDigit(c));
+
+            //foreach (var c in iban)
+            //{
+            //    if (!char.IsLetterOrDigit(c) && c != ' ')
+            //    {
+            //        return false;
+            //    }
+            //}
+
+            //return true;
+        }
+    }
+}
--- Iban/IbanValidatorNL.cs
+++ Iban/IbanValidatorNL.cs
@@ -8,39 +8,16 @@ namespace Iban
         private const string CountryCode = "NL";
         private readonly IBankCodeProvider _provider;
 
-        public IbanValidatorNL(IBankCodeProvider provider)
-        {
+        public IbanValidatorNL(IBankCodeProvider provider) => 
             _provider = provider;
-        }
 
-        public bool Validate(string iban)
-        {
-           
-            return 
-                CheckCountryCode(iban) &&
-                CheckDigits(iban) &&
-                CheckBankCode(iban);
-        }
+        public bool Validate(string iban) =>
+            CheckCountryCode(iban) &&
+            iban.CheckDigits() &&
+            CheckBankCode(iban);
 
         private bool CheckBankCode(string iban) =>
             _provider.BankCodes().Contains(iban.Substring(4, 4));
-
-       
-
-        private static bool CheckDigits(string iban)
-        {
-            return iban.All(c => char.IsLetterOrDigit(c));
-
-            //foreach (var c in iban)
-            //{
-            //    if (!char.IsLetterOrDigit(c) && c != ' ')
-            //    {
-            //        return false;
-            //    }
-            //}
-
-            //return true;
-        }
 
         private static bool CheckCountryCode(string iban) =>
             iban.StartsWith(CountryCode);
