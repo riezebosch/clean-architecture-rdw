ca689ddffe0e995e95f679ea9e540a58af943a71
Manuel Riezebosch <mriezebosch@gmail.com>
2022-10-05 07:33:02 +0000

    feat(adapter): max length code

--- IbanChecker/IbanChecker.BankCodes.EF.IntegrationTests/BankCodeContext.cs
+++ IbanChecker/IbanChecker.BankCodes.EF.IntegrationTests/BankCodeContext.cs
@@ -2,7 +2,7 @@
 
 namespace IbanChecker.BankCodes.EF.IntegrationTests
 {
-    internal class BankCodeContext : DbContext
+    public class BankCodeContext : DbContext
     {
         public BankCodeContext(DbContextOptions options) : base(options)
         {
@@ -11,7 +11,13 @@ namespace IbanChecker.BankCodes.EF.IntegrationTests
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<BankCode>().HasKey(x => x.Code);
+            modelBuilder
+                .Entity<BankCode>()
+                .HasKey(x => x.Code);
+            modelBuilder
+                .Entity<BankCode>()
+                .Property(x => x.Code)
+                .HasMaxLength(4);
         }
 
         public DbSet<BankCode> BankCodes { get; internal set; }
--- IbanChecker/IbanChecker.BankCodes.EF.IntegrationTests/UnitTest1.cs
+++ IbanChecker/IbanChecker.BankCodes.EF.IntegrationTests/UnitTest1.cs
@@ -1,18 +1,32 @@
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
-namespace IbanChecker.BankCodes.EF.IntegrationTests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public async Task Test1Async()
-        {
-            await using var context = new BankCodeContext(new DbContextOptionsBuilder().UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=test").Options);
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
+namespace IbanChecker.BankCodes.EF.IntegrationTests;
 
-            await context.BankCodes.AddAsync(new BankCode { Code = "ABNA" } );
-            await context.SaveChangesAsync();
-        }
+public class UnitTest1 : IClassFixture<DatabaseFixture>
+{
+    private readonly DatabaseFixture database;
+
+    public UnitTest1(DatabaseFixture database) => 
+        this.database = database;
+
+    [Fact]
+    public async Task Test1Async()
+    {
+        await database.Context.BankCodes.AddAsync(new BankCode { Code = "ABNA" });
+        await database.Context.SaveChangesAsync();
+    }
+
+    [Fact]
+    public async Task CodeShouldHaveLength()
+    {
+        await database.Context.BankCodes.AddAsync(new BankCode { Code = "ABNAX" });
+        var act = () => database.Context.SaveChangesAsync();
+
+        var ex = await act.Should()
+            .ThrowAsync<DbUpdateException>();
+        ex.WithInnerException<SqlException>()
+            .WithMessage("*truncate*");
     }
 }
